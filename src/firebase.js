8ed36cced0fd6cccca8e520fba9ec90fc44b0be7
Saptarshi Paul <149397021+Sapta-Dev27@users.noreply.github.com>
2025-07-04 17:36:12 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,17 @@
-import { getAuth } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth";
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyATSX8UQdFde4b8R2D0c2xgBPCUyCnRoeE",
-  authDomain: "trial-ccbaa.firebaseapp.com",
-  projectId: "trial-ccbaa",
-  storageBucket: "trial-ccbaa.firebasestorage.app",
-  messagingSenderId: "736260069514",
-  appId: "1:736260069514:web:411c6a0841d39aa8c6f573",
-  measurementId: "G-80PTPWX0T7"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-
 const app = initializeApp(firebaseConfig);
-
 
 export const auth = getAuth(app);
 export const googleAuthProvider = new GoogleAuthProvider();
